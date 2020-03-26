8adcbe5815ea19dec8529eb35f1b29b138e94689
Tronkai <kaizhu@tron.network>
2020-03-26 13:49:05 +0800

    add test case

--- src/test/java/tron/tronscan/tronscanUI/SendTrc20.java
+++ src/test/java/tron/tronscan/tronscanUI/SendTrc20.java
@@ -25,7 +25,7 @@ public class SendTrc20 {
         }
     }
 
-    @Test(enabled = true)
+    @Test(enabled = false)
     public void testTrc20() throws Exception{
         Step.login(driver);
         {
@@ -37,15 +37,21 @@ public class SendTrc20 {
         driver.findElement(By.cssSelector(".form-group:nth-child(1) .form-control")).click();
         driver.findElement(By.cssSelector(".form-group:nth-child(1) .form-control")).sendKeys("TYvBUrZp7QboQzKhFVMYYkD4jDYsU33aQh");
         //选择TRC20
-//        driver.findElement(By.cssSelector(".ant-select-open svg")).click();
-//        Thread.sleep(200);
-//        driver.findElement(By.xpath("//form/div[2]/div/div/div/div")).click();
-//        driver.findElement(By.cssSelector(".form-group:nth-child(3) .form-control")).sendKeys("0.01");
-//        driver.findElement(By.cssSelector(".form-group:nth-child(4) .form-control")).click();
-//        Thread.sleep(200);
-//        driver.findElement(By.xpath("//form/button")).click();
-//        driver.findElement(By.cssSelector(".btn-primary:nth-child(2)")).click();
-//        Assert.assertEquals(driver.findElement(By.cssSelector(".sweet-alert > h2")).getText(), "Successful Transaction");
+        Thread.sleep(200);
+        driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/div/div/div[2]/form/div[2]/div/div/div/div")).click();
+        Thread.sleep(200);
+        driver.findElement(By.xpath("/html/body/div[3]/div/div"));
+        Thread.sleep(200);
+        driver.findElement(By.xpath("/html/body/div[3]/div/div/div/ul/li[2]/ul/li[1]")).click();
+        Thread.sleep(200);
+        driver.findElement(By.cssSelector(".form-group:nth-child(3) .form-control")).sendKeys("0.01");
+        driver.findElement(By.cssSelector(".form-group:nth-child(4) .form-control")).click();
+        driver.findElement(By.cssSelector(".form-group:nth-child(4) .form-control")).sendKeys("TRC 20");
+        Thread.sleep(200);
+        driver.findElement(By.xpath("//form/button")).click();
+        driver.findElement(By.cssSelector(".btn-primary:nth-child(2)")).click();
+        Thread.sleep(300);
+        Assert.assertEquals(driver.findElement(By.cssSelector(".sweet-alert > h2")).getText(), "Successful Transaction");
     }
 
     @AfterMethod(enabled = true)
