0223f8fcf5f59206a4ea57ea26b991141351609b
jh <919515326@qq.com>
2020-03-26 16:10:06 +0800

    edit case

--- src/test/java/tron/tronscan/tronscanPart/Top_statisticsTest.java
+++ src/test/java/tron/tronscan/tronscanPart/Top_statisticsTest.java
@@ -21,22 +21,32 @@ public class Top_statisticsTest {
             try {
                 driver = webBrowser.startChrome(URL);
             } catch (Exception e) {
+                System.out.println(e);
             }
         }
 
     @Test(enabled = true,description = "最佳数据-概览")
     public void testTop() throws Exception{
-        driver.findElement(By.cssSelector("div.card-header.list-style-body__header > ul > li:nth-child(1) > a")).click();
+      //  driver.findElement(By.cssSelector("div.card-header.list-style-body__header > ul > li:nth-child(1) > a")).click();
         driver.findElement(By.cssSelector("div.card-header.list-style-body__header > ul > li:nth-child(1) > a > span > span")).getText();
         //hours;day;week;
         driver.findElement(By.cssSelector("div.time-filter.d-flex.justify-content-between > ul > li:nth-child(1) > span")).click();
         driver.findElement(By.cssSelector("div.time-filter.d-flex.justify-content-between > ul > li:nth-child(2) > span")).click();
         driver.findElement(By.cssSelector("div.time-filter.d-flex.justify-content-between > ul > li:nth-child(3) > span")).click();
-        Thread.sleep(200);
+        //Thread.sleep(200);
         //four modules
-        driver.findElement(By.cssSelector("div.justify-content-between.mt-0 > div:nth-child(1) > div.justify-content-between > div:nth-child(1) > span")).getText().isEmpty();
-        driver.findElement(By.cssSelector("div.justify-content-between.mt-0 > div:nth-child(2) > div.justify-content-between > div:nth-child(1) > span")).getText().isEmpty();
+        Thread.sleep(100);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div[1]/div[1]"));
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div[1]/div[1]/div[1]/div[1]/span")).getText().isEmpty();
+
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div[1]/div[2]"));
+        Thread.sleep(100);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div[1]/div[2]/div[1]/div[1]/span")).getText().isEmpty();
+        Thread.sleep(100);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div[2]/div[1]"));
         driver.findElement(By.cssSelector("div.data-overview-list > div:nth-child(2) > div:nth-child(1) > div.justify-content-between > div:nth-child(1) > span")).getText().isEmpty();
+        Thread.sleep(100);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div[2]/div[2]"));
         driver.findElement(By.cssSelector("div.data-overview-list > div:nth-child(2) > div:nth-child(2) > div.justify-content-between > div:nth-child(1) > span")).getText().isEmpty();
 
     }
@@ -51,11 +61,23 @@ public class Top_statisticsTest {
         driver.findElement(By.cssSelector("div.time-filter.justify-content-between > ul > li:nth-child(3) > span")).click();
         //Thread.sleep(200);
         //six modules
+        Thread.sleep(100);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div/div[1]"));
         driver.findElement(By.cssSelector("div.card-body.list-style-body__body > div > div.top-data > div > div:nth-child(1) > div > h2 > span")).getText().isEmpty();
+        Thread.sleep(100);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div/div[2]"));
         driver.findElement(By.cssSelector("div.card-body.list-style-body__body > div > div.top-data > div > div:nth-child(2) > div > h2 > span")).getText().isEmpty();
+        Thread.sleep(100);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div/div[3]"));
         driver.findElement(By.cssSelector("div.card-body.list-style-body__body > div > div.top-data > div > div:nth-child(3) > div > h2 > span")).getText().isEmpty();
+        Thread.sleep(100);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div/div[4]"));
         driver.findElement(By.cssSelector("div.card-body.list-style-body__body > div > div.top-data > div > div:nth-child(4) > div > h2 > span")).getText().isEmpty();
+        Thread.sleep(100);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div/div[5]"));
         driver.findElement(By.cssSelector("div.card-body.list-style-body__body > div > div.top-data > div > div:nth-child(5) > div > h2 > span")).getText().isEmpty();
+        Thread.sleep(100);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div/div[6]"));
         driver.findElement(By.cssSelector("div.card-body.list-style-body__body > div > div.top-data > div > div:nth-child(6) > div > h2 > span")).getText().isEmpty();
 
     }
@@ -69,15 +91,59 @@ public class Top_statisticsTest {
         driver.findElement(By.cssSelector("div.time-filter.justify-content-between > ul > li:nth-child(1) > span")).click();
         driver.findElement(By.cssSelector("div.time-filter.justify-content-between > ul > li:nth-child(1) > span")).click();
         driver.findElement(By.cssSelector("div.time-filter.justify-content-between > ul > li:nth-child(3) > span")).click();
-        Thread.sleep(200);
+        //Thread.sleep(200);
         //four modules
-        driver.findElement(By.cssSelector("div.top-data > div.ant-row.data-token > div:nth-child(1) > div > h2 > span")).getText().isEmpty();
-        driver.findElement(By.cssSelector("div.top-data > div.ant-row.data-token > div:nth-child(2) > div > h2 > span")).getText().isEmpty();
-        driver.findElement(By.cssSelector("div.top-data > div.ant-row.data-token > div:nth-child(3) > div > h2 > span")).getText().isEmpty();
-        driver.findElement(By.cssSelector("div.top-data > div.ant-row.data-token > div:nth-child(4) > div > h2 > span")).getText().isEmpty();
+        Thread.sleep(100);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div[2]/div[1]"));
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div[2]/div[1]/div/h2/span")).getText().isEmpty();
+        Thread.sleep(100);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div[2]/div[2]"));
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div[2]/div[2]/div/h2/span")).getText().isEmpty();
+        Thread.sleep(100);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div[2]/div[3]"));
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div[2]/div[3]/div/h2/span")).getText().isEmpty();
+        Thread.sleep(100);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div[2]/div[4]"));
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div[2]/div[4]/div/h2/span")).getText().isEmpty();
 
     }
 
+    @Test(enabled = true,description = "最佳数据-合约")
+    public void testTopContract() throws Exception{
+        driver.findElement(By.cssSelector(" div.card-header.list-style-body__header > ul > li:nth-child(4) > a")).click();
+        driver.findElement(By.cssSelector("div.card-header.list-style-body__header > ul > li:nth-child(4) > a > span > span")).getText();
+        //hours;day;week;
+        driver.findElement(By.cssSelector("div.time-filter.justify-content-between > ul > li:nth-child(1) > span")).click();
+        driver.findElement(By.cssSelector("div.time-filter.justify-content-between > ul > li:nth-child(1) > span")).click();
+        driver.findElement(By.cssSelector("div.time-filter.justify-content-between > ul > li:nth-child(3) > span")).click();
+        //four modules
+
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div/div[1]"));
+        Thread.sleep(100);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div/div[1]/div/h2/span")).getText().isEmpty();
+        Thread.sleep(100);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div/div[2]"));
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div/div[2]/div/h2/span")).getText().isEmpty();
+        Thread.sleep(100);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div/div[3]"));
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/div/div[3]/div/h2/span")).getText().isEmpty();
+
+    }
+
+    @Test(enabled = true,description = "最佳数据-资源")
+    public void testTopResource() throws Exception{
+        driver.findElement(By.cssSelector("div.card-header.list-style-body__header > ul > li:nth-child(5) > a")).click();
+        driver.findElement(By.cssSelector("div.card-header.list-style-body__header > ul > li:nth-child(5) > a > span > span")).getText();
+        //hours;day;week;
+        driver.findElement(By.cssSelector("div.time-filter.justify-content-between > ul > li:nth-child(1) > span")).click();
+        driver.findElement(By.cssSelector("div.time-filter.justify-content-between > ul > li:nth-child(1) > span")).click();
+        driver.findElement(By.cssSelector("div.time-filter.justify-content-between > ul > li:nth-child(3) > span")).click();
+        //four modules
+        Thread.sleep(100);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]"));
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/div[2]/h4[1]/span")).getText().isEmpty();
+
+    }
     @AfterMethod(enabled = true)
     public void end() throws Exception {
         WebBrowser.tearDownBrowser();
