9818b9e5cb9f5bb97545c8503c8f4100134d8d4c
Tronkai <kaizhu@tron.network>
2020-04-13 23:34:00 +0800

    add test case

--- src/test/java/tron/common/utils/SqlDjed.java
+++ src/test/java/tron/common/utils/SqlDjed.java
@@ -83,7 +83,7 @@ public class SqlDjed implements IReporter{
                 }
                 if (result.getStatus() == 1) {
                     success.add(result.getMethod().getMethodName());
-                } else if (result.getStatus() == 0) {
+                } else  {
                     fail.add(result.getMethod().getMethodName());
                 }
 //                sb.append(result.getTestClass().getRealClass().getName())
--- src/test/java/tron/common/utils/SqlTronscanUI.java
+++ src/test/java/tron/common/utils/SqlTronscanUI.java
@@ -71,7 +71,7 @@ public class SqlTronscanUI implements IReporter{
             }
             if (result.getStatus() == 1) {
                 success.add(result.getMethod().getMethodName());
-            } else if (result.getStatus() == 0) {
+            } else{
                 fail.add(result.getMethod().getMethodName());
             }
 //                sb.append(result.getTestClass().getRealClass().getName())
