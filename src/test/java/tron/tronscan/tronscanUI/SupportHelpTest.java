4ec1c9b4ddbe7d857cccba69771eee5d4ba0ae15
jh <919515326@qq.com>
2020-04-24 10:28:29 +0800

    edit case

---
+++ src/test/java/tron/tronscan/tronscanUI/SupportHelpTest.java
@@ -0,0 +1,54 @@
+package tron.tronscan.tronscanUI;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import tron.common.utils.Configuration;
+import tron.common.utils.MyIRetryAnalyzer;
+import tron.common.utils.WebBrowser;
+
+public class SupportHelpTest {
+    private String tronScanNode = Configuration.getByPath("testng.conf")
+            .getString("tronscanIP");
+    private  String URL = "https://"+tronScanNode+"/#/";
+    WebBrowser webBrowser = new WebBrowser();
+    public static WebDriver driver;
+    @BeforeMethod(enabled = true)
+    public void start() throws Exception {
+        try {
+            driver = webBrowser.startChrome(URL);
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Test(enabled = true,description = "支持与帮助--新手引导",retryAnalyzer = MyIRetryAnalyzer.class)
+    public void testHelp() throws Exception{
+        driver.findElement(By.cssSelector("#navbar-top > ul > li:nth-child(7) > span > span > span > span")).click();
+        driver.findElement(By.cssSelector("#navbar-top > ul > li:nth-child(7) > div > div:nth-child(3) > h6 > span")).getText().isEmpty();
+        //新手引导
+        driver.findElement(By.cssSelector("#navbar-top > ul > li:nth-child(7) > div > div:nth-child(3) > a:nth-child(3) > span")).getText().isEmpty();
+        driver.findElement(By.cssSelector("#navbar-top > ul > li:nth-child(7) > div > div:nth-child(3) > a:nth-child(3) > span")).click();
+//        driver.findElement(By.cssSelector("body > main > div.container > div > div > header > h1")).getText().isEmpty();
+        driver.close();
+    }
+
+    @Test(enabled = true,description = "支持与帮助--常见问题",retryAnalyzer = MyIRetryAnalyzer.class)
+    public void testFAQ() throws Exception{
+        driver.findElement(By.cssSelector("#navbar-top > ul > li:nth-child(7) > span > span > span > span")).click();
+        driver.findElement(By.cssSelector("#navbar-top > ul > li:nth-child(7) > div > div:nth-child(3) > h6 > span")).getText().isEmpty();
+        //常见问题
+        driver.findElement(By.cssSelector("#navbar-top > ul > li:nth-child(7) > div > div:nth-child(3) > a:nth-child(4) > span")).getText().isEmpty();
+        driver.findElement(By.cssSelector("#navbar-top > ul > li:nth-child(7) > div > div:nth-child(3) > a:nth-child(4) > span")).click();
+
+        driver.close();
+    }
+
+    @AfterMethod(enabled = true)
+    public void end() throws Exception {
+        WebBrowser.tearDownBrowser();
+    }
+}
