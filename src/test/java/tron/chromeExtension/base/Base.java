c08720bbbab0c080f75e61d18d63e6ca51972da7
pangpangfeng <7577288@qq.com>
2020-05-07 16:32:20 +0800

    Add chrome extension send trx/trc10/trc20 test case

---
+++ src/test/java/tron/chromeExtension/base/Base.java
@@ -0,0 +1,107 @@
+package tron.chromeExtension.base;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeTest;
+import tron.common.utils.Configuration;
+import tron.chromeExtension.pages.*;
+
+public class Base {
+
+    public ChromeDriver DRIVER;
+    static String UNIQUEID = Configuration.getByPath("testng.conf")
+      .getString("chromeExtension.uniqueId");
+    static String PAGEPATH = Configuration.getByPath("testng.conf")
+      .getString("chromeExtension.pagePath");
+    static String userDataDir = Configuration.getByPath("testng.conf")
+      .getString("chromeExtension.userDataDir");
+    static String extensionDir = Configuration.getByPath("testng.conf")
+      .getString("chromeExtension.extensionDir");
+    static String password = Configuration.getByPath("testng.conf")
+      .getString("chromeExtension.password");
+    public String accountAddress001 = Configuration.getByPath("testng.conf")
+      .getString("chromeExtension.accountAddress001");
+    public String accountKey001 = Configuration.getByPath("testng.conf")
+      .getString("chromeExtension.accountKey001");
+    public String accountAddress002 = Configuration.getByPath("testng.conf")
+      .getString("chromeExtension.accountAddress002");
+    public String accountKey002 = Configuration.getByPath("testng.conf")
+      .getString("chromeExtension.accountKey002");
+    public String URL = "chrome-extension://" + UNIQUEID + PAGEPATH;
+    ChromeOptions OPTION = new ChromeOptions ();
+    Integer retryLoginTimes = 5;
+
+
+
+    public void setUpChromeDriver()throws Exception {
+      killChromePid();
+      try {
+        OPTION.addArguments("--user-data-dir=" + userDataDir);
+        OPTION.addArguments ("load-extension=" + extensionDir);
+        OPTION.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+        OPTION.addArguments("--enable-extensions");
+        OPTION.addArguments("--verbose");
+        DRIVER = new ChromeDriver(OPTION);
+      } catch (Exception e) {
+        e.printStackTrace();
+
+      }
+    }
+
+    public boolean loginAccount() throws Exception{
+      while (retryLoginTimes > 0) {
+        try {
+          retryLoginTimes--;
+          DRIVER.get(URL);
+          try {
+            if (new MainPage(DRIVER).send_btn.isEnabled()){
+              return true;
+            }
+          } catch (Exception e) {
+          }
+          DRIVER.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+          LoginPage loginPage = new LoginPage(DRIVER);
+          loginPage.password_input.sendKeys(password);
+          DRIVER.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+          loginPage.login_btn.click();
+          DRIVER.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+          Float trxBalance = Float.valueOf(loginPage.trxBalance.getText());
+          if (trxBalance > 0) {
+            return true;
+          }
+          DRIVER.quit();
+        } catch (Exception e) {
+          e.printStackTrace();
+          DRIVER.quit();
+          setUpChromeDriver();
+        }
+      }
+      return false;
+
+  }
+
+
+
+    public void logoutAccount() throws Exception {
+      DRIVER.close();
+    }
+
+
+    public double getBalanceFromSelectionBtn(String string) {
+      System.out.println("string:" + string);
+      int left = string.indexOf('(');
+      int right = string.indexOf(')');
+      return Double.valueOf(string.substring(left+1,right));
+    }
+
+    public  static void killChromePid() throws IOException {
+      Process process = Runtime.getRuntime().exec("sh kill_chrome.sh");
+    }
+
+}
