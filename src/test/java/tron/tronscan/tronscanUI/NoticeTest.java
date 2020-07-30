266a59e734c334c272e7975d4b9d599c728a6013
jh <919515326@qq.com>
2020-07-30 17:09:14 +0800

    edit case

--- src/test/java/tron/tronscan/tronscanUI/NoticeTest.java
+++ src/test/java/tron/tronscan/tronscanUI/NoticeTest.java
@@ -23,118 +23,67 @@ public class NoticeTest {
         } catch (Exception e) {
         }
     }
-    @Test(enabled = true,description = "广播栏")
+    @Test(enabled = true,description = "广播栏",retryAnalyzer = MyIRetryAnalyzer.class)
     public void testTop() throws Exception{
-        driver.findElement(By.cssSelector("#navbar-top > ul > li:nth-child(1) > span > span > a > span > span")).click();
-        Boolean real_display = driver.findElement(By.cssSelector("#root > main > div.homeNotice> div > div > div > div.hidden-mobile > div > img")).isDisplayed();
-        if(real_display){
-           //广播三条数据
-           //driver.findElement(By.xpath("//*[@id=\"root\"]/main/div[1]/div/div[1]/div/div/div"));
-           //
-           driver.findElement(By.cssSelector("#root > main > div.homeNotice > div > div > div > div.hidden-mobile > div > div > a.item-0.item > span.title")).getText().isEmpty();
-           driver.findElement(By.cssSelector("#root > main > div.homeNotice > div > div > div > div.hidden-mobile > div > div > a.item-0.item > span.date")).getText().isEmpty();
-            //
-           driver.findElement(By.cssSelector("#root > main > div.homeNotice> div > div > div > div.hidden-mobile > div > div > a.item-1.item > span.title")).getText().isEmpty();
-           driver.findElement(By.cssSelector("#root > main > div.homeNotice> div > div > div > div.hidden-mobile > div > div > a.item-1.item > span.date")).getText().isEmpty();
-            //
-           driver.findElement(By.cssSelector("#root > main > div.homeNotice > div > div > div > div.hidden-mobile > div > div > a.item-2.item > span.title")).getText().isEmpty();
-           driver.findElement(By.cssSelector("#root > main > div.homeNotice > div > div > div > div.hidden-mobile > div > div > a.item-2.item > span.date")).getText().isEmpty();
-           //more
-           driver.findElement(By.cssSelector("#root > main > div.homeNotice > div > div > div > div.hidden-mobile > div > a > span")).click();
-           driver.close();
-       }
-
-    }
+        //转义跳转-给出地址跳转的，后期修复
+       driver.findElement(By.cssSelector("#navbar-top > ul > li:nth-child(1) > span > span > a > span > span")).click();
+        Thread.sleep(200);
+         //
+//       driver.findElement(By.cssSelector("#root > main > div.homeNotice > div > div > div > div.hidden-mobile > div > div > a.item-0.item > span.title")).getText().isEmpty();
+//       driver.findElement(By.cssSelector("#root > main > div.homeNotice > div > div > div > div.hidden-mobile > div > div > a.item-0.item > span.date")).getText().isEmpty();
+        //
+       driver.findElement(By.cssSelector("#root > main > div.homeNotice> div > div > div > div.hidden-mobile > div > div > a.item-1.item > span.title")).getText().isEmpty();
+       driver.findElement(By.cssSelector("#root > main > div.homeNotice> div > div > div > div.hidden-mobile > div > div > a.item-1.item > span.date")).getText().isEmpty();
+        //
+       driver.findElement(By.cssSelector("#root > main > div.homeNotice > div > div > div > div.hidden-mobile > div > div > a.item-2.item > span.title")).getText().isEmpty();
+       driver.findElement(By.cssSelector("#root > main > div.homeNotice > div > div > div > div.hidden-mobile > div > div > a.item-2.item > span.date")).getText().isEmpty();
+       //more
+       driver.findElement(By.cssSelector("#root > main > div.homeNotice > div > div > div > div.hidden-mobile > div > a > span")).click();
+       driver.close();
+   }
 
     @Test(enabled = true,description = "数据栏",retryAnalyzer = MyIRetryAnalyzer.class)
     public void topDataTest() throws Exception {
-        Boolean onlinedata_display = driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div.text-center.mr-0")).isDisplayed();
-        if (onlinedata_display){
-            //线上节点
-            driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div.text-center.mr-0 > div > div > div:nth-child(2) > p > span")).getText().isEmpty();
-            driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div.text-center.mr-0 > div > div > div:nth-child(2) > h2 > span")).getText().isEmpty();
-            Thread.sleep(200);
-            //区块高度
-            driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div.text-center.mr-0 > div > div > div:nth-child(3) > p > span")).getText().isEmpty();
-            driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div.text-center.mr-0 > div > div > div:nth-child(3) > h2 > span")).getText().isEmpty();
-            Thread.sleep(200);
-            //峰值
-            driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div.text-center.mr-0 > div > div > div:nth-child(4) > p > span")).getText().isEmpty();
-            driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div.text-center.mr-0 > div > div > div:nth-child(4) > div > h2 >span:nth-child(1) > span")).getText().isEmpty();
-            driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div.text-center.mr-0 > div > div > div:nth-child(4) > div > h2 >span:nth-child(2) > span")).getText().isEmpty();
-            Thread.sleep(200);
-            //一天交易
-            driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div.text-center.mr-0 > div > div > div:nth-child(5) > p > span")).getText().isEmpty();
-            driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div.text-center.mr-0 > div > div > div:nth-child(5) > h2 > span")).getText().isEmpty();
-            Thread.sleep(200);
-            //总账户数
-            driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div.text-center.mr-0 > div > div > div:nth-child(6) > p > span")).getText().isEmpty();
-            driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div.text-center.mr-0> div > div > div:nth-child(6) > h2 > span")).getText().isEmpty();
-
-        }
-        //数据详情
+        //转义跳转-给出地址跳转的，后期修复
+        driver.findElement(By.cssSelector("#navbar-top > ul > li:nth-child(1) > span > span > a > span > span")).click();
         Thread.sleep(200);
-        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div[1]/div/div[2]/div/div/span/span")).click();
-        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div[1]/div/div[2]/div/div/div[2]/div[1]/span/span")).getText().isEmpty();
-        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div[1]/div/div[2]/div/div/div[2]/div[3]/span/span")).getText().isEmpty();
+        //最新区块
+        driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div:nth-child(1) > div > div > div.text-center > div > div > div:nth-child(1) > a > h2 > span")).getText().isEmpty();
+        driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div:nth-child(1) > div > div > div.text-center> div > div > div:nth-child(1) > a > p > span")).getText().isEmpty();
+        Thread.sleep(200);
+        //交易总数
+        driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div:nth-child(1) > div > div > div.text-center > div > div > div:nth-child(2) > h2 > span")).getText().isEmpty();
+        driver.findElement(By.cssSelector(" div:nth-child(2) > div > div > div:nth-child(1) > div > div > div.text-center > div > div > div:nth-child(2) > p > span")).getText().isEmpty();
+        Thread.sleep(200);
+        //账户总数
+        driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div:nth-child(1) > div > div > div.text-center > div > div > div:nth-child(3) > h2 > span")).getText().isEmpty();
+        driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div:nth-child(1) > div > div > div.text-center> div > div > div:nth-child(3) > p > span")).getText().isEmpty();
+        //driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div.text-center.mr-0 > div > div > div:nth-child(4) > div > h2 >span:nth-child(2) > span")).getText().isEmpty();
+        Thread.sleep(200);
+        //TRX冻结量
+        driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div:nth-child(1) > div > div > div.text-center> div > div > div:nth-child(6) > a > h2 > span")).getText().isEmpty();
+        driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div:nth-child(1) > div > div > div.text-center > div > div > div:nth-child(6) > a > p > span")).getText().isEmpty();
+        Thread.sleep(200);
+        //合约总数
+        driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div:nth-child(1) > div > div > div.text-center > div > div > div:nth-child(7) > h2 > span")).getText().isEmpty();
+        driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div:nth-child(1) > div > div > div.text-center > div > div > div:nth-child(7) > p > span")).getText().isEmpty();
+        //通证总数
+        driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div:nth-child(1) > div > div > div.text-center > div > div > div:nth-child(8) > a > h2 > span")).getText().isEmpty();
+        driver.findElement(By.cssSelector("div:nth-child(2) > div > div > div:nth-child(1) > div > div > div.text-center > div > div > div:nth-child(8) > a > p > span")).getText().isEmpty();
+
+        //展开
+        Thread.sleep(200);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div[2]/div/div/div[1]/div/div/span/span")).click();
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div[2]/div/div/div[1]/div/div/div[2]/div[1]/span/span")).getText().isEmpty();
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div[2]/div/div/div[1]/div/div/div[2]/div[3]/span/span")).getText().isEmpty();
         Thread.sleep(200);
         //收起
-        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div[1]/div/div[2]/div/div/span/span")).click();
+        driver.findElement(By.xpath("//*[@id=\"root\"]/main/div[2]/div/div/div[1]/div/div/span/span")).click();
         driver.close();
 
     }
-    @Test(enabled = true,description = "过去14天交易数",retryAnalyzer = MyIRetryAnalyzer.class)
-    public void day14_transactions() throws Exception{
-        Boolean transactions_display = driver.findElement(By.cssSelector("div:nth-child(1) > div > div>div >div> div > div > svg > rect.highcharts-plot-background")).isDisplayed();
-        if (transactions_display) {
-            driver.findElement(By.cssSelector("div:nth-child(1) > div > div> div > div >div >div > svg > text.highcharts-title > tspan")).getText().isEmpty();
-            Thread.sleep(200);
-            driver.findElement(By.cssSelector("div:nth-child(1) > div > div> div > div >div >div >svg > g.highcharts-legend > g > g > g.highcharts-series-0 > text > tspan")).getText().isEmpty();
-            driver.findElement(By.cssSelector("div:nth-child(1) > div > div> div > div >div >div >svg > g.highcharts-legend > g > g > g.highcharts-series-1 > text > tspan")).getText().isEmpty();
-            driver.findElement(By.cssSelector("div:nth-child(1) > div > div> div > div >div >div >svg > g.highcharts-legend > g > g > g.highcharts-series-2 > text > tspan")).getText().isEmpty();
-            Thread.sleep(200);
-            driver.findElement(By.cssSelector("div:nth-child(1) > div > div>div >div> div > div > svg > g.highcharts-exporting-group > g ")).click();
-            driver.close();
-        }
 
-    }
 
-    @Test(enabled = true,description = "过去14天账户数",retryAnalyzer = MyIRetryAnalyzer.class)
-    public void day14_accounts() throws Exception{
-        Boolean accounts_display = driver.findElement(By.cssSelector("div:nth-child(2) > div > div>div >div> div > div > svg > rect.highcharts-plot-background")).isDisplayed();
-        if (accounts_display) {
-            driver.findElement(By.cssSelector("div:nth-child(2) > div > div> div > div >div >div > svg > text.highcharts-title > tspan")).getText().isEmpty();
-            Thread.sleep(200);
-            driver.findElement(By.cssSelector("div:nth-child(2) > div > div> div > div >div >div >svg > g.highcharts-legend > g > g > g.highcharts-series-0 > text > tspan")).getText().isEmpty();
-            driver.findElement(By.cssSelector("div:nth-child(2) > div > div> div > div >div >div >svg > g.highcharts-legend > g > g > g.highcharts-series-1 > text > tspan")).getText().isEmpty();
-            driver.findElement(By.cssSelector("div:nth-child(2) > div > div> div > div >div >div >svg > g.highcharts-legend > g > g > g.highcharts-series-2 > text > tspan")).getText().isEmpty();
-            Thread.sleep(200);
-            //driver.findElement(By.cssSelector("div:nth-child(2) > div > div>div >div> div > div > svg > g.highcharts-exporting-group > g")).click();
-            driver.close();
-        }
-    }
-
-    @Test(enabled = true,description = "账户",retryAnalyzer = MyIRetryAnalyzer.class)
-    public void top_accounts() throws Exception{
-        Boolean topacc_display = driver.findElement(By.cssSelector("div.row.indxe-page-bottom-sec-pc > div.mt-md-0.text-center > div")).isDisplayed();
-        if (topacc_display){
-            driver.findElement(By.cssSelector("div.row.indxe-page-bottom-sec-pc > div.mt-md-0.text-center > div > div > h5 > span")).getText().isEmpty();
-            driver.findElement(By.cssSelector("li:nth-child(12) > div > div > div.ml-auto.text-right.d-flex > div.list-item-word > span.d-inline-block > span")).getText().isEmpty();
-            //
-            Thread.sleep(200);
-            driver.findElement ( By.cssSelector("li:nth-child(12) > div > div > div.ml-auto.text-right.d-flex > div.list-item-word > span:nth-child(2)")).getText().isEmpty();
-            driver.findElement(By.cssSelector("div.indxe-page-bottom-sec-pc > div.mt-md-0.text-center > div > div > a > span")).click();
-        }
-    }
-
-    @Test(enabled = true,description = "转账",retryAnalyzer = MyIRetryAnalyzer.class)
-    public void top_transfer() throws Exception{
-        Boolean topacc_display = driver.findElement(By.cssSelector("div.row.indxe-page-bottom-sec-pc > div:nth-child(2) > div")).isDisplayed();
-        if (topacc_display){
-            driver.findElement(By.cssSelector("div:nth-child(2) > div > div.card-header.bg-tron-light.d-flex > h5 > span")).getText().isEmpty();
-            driver.findElement(By.cssSelector("div.indxe-page-bottom-sec-pc > div:nth-child(2) > div > div.card-header.bg-tron-light.d-flex > a > span")).click();
-        }
-    }
     @AfterMethod(enabled = true)
     public void end() throws Exception {
         WebBrowser.tearDownBrowser();
