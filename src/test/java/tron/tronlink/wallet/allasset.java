5049ec98cdce37e6042a0ce59b3296556f7f83f6
sophiawang <1975054324@qq.com>
2020-09-21 13:25:00 +0800

    fix cases

--- src/test/java/tron/tronlink/wallet/allasset.java
+++ src/test/java/tron/tronlink/wallet/allasset.java
@@ -21,13 +21,14 @@ public class allasset {
   @Test(enabled = true)
   public void dapp_classify(){
 
-    response = TronlinkApiList.allasset("TN2jfdYCX9vvozqjwVvPjMd7vRj8HKyxUe");
+    response = TronlinkApiList.allasset("TAVNk5hkaPNJcTf6TvJVgBWEaRhuiHE5Ab");
     Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
     responseContent = TronlinkApiList.parseJsonObResponseContent(response);
     responseArrayContent = responseContent.getJSONArray("data");
     //data object
-    for (Object json:responseArrayContent
-    ) {
+    boolean TG4 =false,TG12 =false,TG18 =false;;
+
+    for (Object json:responseArrayContent) {
       JSONObject jsonObject = (JSONObject) JSON.toJSON(json);
       Assert.assertTrue(jsonObject.containsKey("type"));
       Assert.assertTrue(jsonObject.containsKey("top"));
@@ -36,6 +37,14 @@ public class allasset {
       Assert.assertTrue(jsonObject.containsKey("shortName"));
       Assert.assertTrue(jsonObject.containsKey("id"));
       Assert.assertTrue(jsonObject.containsKey("contractAddress"));
+      String contractAddress = jsonObject.getString("contractAddress");
+      if("TKTwvJhHAfiUNMrdnWoYkoUY9meYnTgtdk".equals(contractAddress)){
+        TG12 = true;
+      }else if("TWPGEQ6iD4t2BrSMi1Lwj8Pha5GpGEXLay".equals(contractAddress)){
+        TG18 = true;
+      }else if("TQ8kZgvhcP6cHZTPLEJbBojsycZKb8V2wL".equals(contractAddress)){
+        TG4 = true;
+      }
       Assert.assertTrue(jsonObject.containsKey("balance"));
       Assert.assertTrue(jsonObject.containsKey("totalBalance"));
       Assert.assertTrue(jsonObject.containsKey("logoUrl"));
@@ -46,6 +55,7 @@ public class allasset {
       Assert.assertTrue(jsonObject.containsKey("inMainChain"));
       Assert.assertTrue(jsonObject.containsKey("inSideChain"));
     }
+    Assert.assertTrue(TG4&&TG12&&TG18);
     System.out.println(responseArrayContent.size());
   }
 }
