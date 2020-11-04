3d547d23bf0334dca88bcee69fee23aa5654efd2
sophiawang <1975054324@qq.com>
2020-11-04 12:15:20 +0800

    fix dapp history

--- src/test/java/tron/tronlink/dapp/Dapp_History.java
+++ src/test/java/tron/tronlink/dapp/Dapp_History.java
@@ -1,13 +1,11 @@
 package tron.tronlink.dapp;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.http.HttpResponse;
 import org.junit.Assert;
 import org.testng.annotations.Test;
 import tron.common.TronlinkApiList;
-import tron.common.utils.Configuration;
 import tron.tronlink.base.TronlinkBase;
 
 import java.util.HashMap;
@@ -19,18 +17,16 @@ public class Dapp_History extends TronlinkBase {
     private JSONArray responseArrayContent;
     private JSONObject targetContent;
     private HttpResponse response;
-    private String node = Configuration.getByPath("testng.conf")
-            .getStringList("tronlink.ip.list")
-            .get(0);
 
     @Test(enabled = true)
     public void history() {
         Map<String, String> params = new HashMap<>();
-        params.put("address","TN2jfdYCX9vvozqjwVvPjMd7vRj8HKyxUe");
+        params.put("address","TAVNk5hkaPNJcTf6TvJVgBWEaRhuiHE5Ab");
         response = TronlinkApiList.history(params);
         Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
         responseContent = TronlinkApiList.parseJsonObResponseContent(response);
-        //data object
+        responseArrayContent = responseContent.getJSONArray("data");
+        Assert.assertEquals(1,responseArrayContent.size());
     }
 
 }
