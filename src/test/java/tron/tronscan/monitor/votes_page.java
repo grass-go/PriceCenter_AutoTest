423b8c6fd7b06e87c1185389bbacd4b5f58d3247
Tronkai <kerwin.zhu@tron.network>
2020-12-01 13:27:10 +0800

    change test case

--- src/test/java/tron/tronscan/monitor/votes_page.java
+++ src/test/java/tron/tronscan/monitor/votes_page.java
@@ -53,7 +53,7 @@ public class votes_page {
             Assert.assertTrue(responseArrayContent.getJSONObject(i).getLong("realTimeRanking") > 0);
             Assert.assertTrue(responseArrayContent.getJSONObject(i).getLong("lastCycleVotes") <= lastCycleVotesNum);
             Assert.assertTrue(responseArrayContent.getJSONObject(i).getLong("realTimeVotes") > 10000000L);
-            Assert.assertTrue(responseArrayContent.getJSONObject(i).getLong("lastCycleVotes") > 10000000L);
+            Assert.assertTrue(responseArrayContent.getJSONObject(i).getLong("lastCycleVotes") > 1000000L);
             Assert.assertTrue(responseArrayContent.getJSONObject(i).containsKey("votesPercentage"));
             //annualizedRate
             Assert.assertTrue(Double.valueOf(responseArrayContent.getJSONObject(i).get("annualizedRate").toString()) >= 0);
