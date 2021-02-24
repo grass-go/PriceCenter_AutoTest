c9aa6c5579324e8a5a4fe6e568a9960880d4491e
Tronkai <kerwin.zhu@tron.network>
2021-02-24 12:00:55 +0800

    change test case

--- src/test/java/tron/tronscan/monitor/representatives_page.java
+++ src/test/java/tron/tronscan/monitor/representatives_page.java
@@ -62,7 +62,7 @@ public class representatives_page {
         Assert.assertTrue(!highestContent.getString("name").isEmpty());
         Assert.assertTrue(!highestContent.getString("url").isEmpty());
         Assert.assertTrue(Double.valueOf(highestContent.getString("missedTotal")) >= 0);
-        Assert.assertTrue(Double.valueOf(highestContent.getString("producedTotal")) > 1000);
+        Assert.assertTrue(Double.valueOf(highestContent.getString("producedTotal")) >= 0);
         Assert.assertTrue(Double.valueOf(highestContent.getString("producePercentage")) <= 100);
         //lowestEfficiency
         // 出块效率最低
