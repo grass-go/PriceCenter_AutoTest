586eeba79c9346c9c9c8cc7192f8a8328ff325c5
wqq <lanyu@wqqs-MacBook-Pro.local>
2021-06-05 19:27:22 +0800

    add 721 address for config

--- src/test/java/tron/tronlink/base/TronlinkBase.java
+++ src/test/java/tron/tronlink/base/TronlinkBase.java
@@ -20,6 +20,9 @@ public class TronlinkBase {
     public  String queryAddressTH48 = Configuration.getByPath("testng.conf").getString("tronlink.queryAddressTH48");
     public  String addressNewAsset = Configuration.getByPath("testng.conf").getString("tronlink.addressNewAsset");
     public  String addressNewAsset41 = Configuration.getByPath("testng.conf").getString("tronlink.addressNewAsset41");
+    public String address721_B58 = Configuration.getByPath("testng.conf").getString("tronlink.address721_B58");
+    public String address721_Hex = Configuration.getByPath("testng.conf").getString("tronlink.address721_Hex");
+
     public static GetSign getSign = new GetSign();
     public String nonce = "12345";
     public String secretId = "SFSUIOJBFMLKSJIF";
