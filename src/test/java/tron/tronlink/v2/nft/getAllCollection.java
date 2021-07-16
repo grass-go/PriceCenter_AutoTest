c693510ba88c68d4ad982f238c0be800bf7aa792
wqq <lanyu@wqqs-MacBook-Pro.local>
2021-07-16 12:06:36 +0800

    enable getAllCollectionTest002 since cache 45000 issue has resolved.

--- src/test/java/tron/tronlink/v2/nft/getAllCollection.java
+++ src/test/java/tron/tronlink/v2/nft/getAllCollection.java
@@ -72,7 +72,7 @@ public class getAllCollection extends TronlinkBase {
   //            2. 相当于对此账户下首页展示的收藏品做全对比。
   //            3. 同时也校验了关注币的排序。
   @SneakyThrows
-  @Test(enabled=false)
+  @Test(enabled=true)
   public void getAllCollectionTest002(){
     // read expected json
     char cbuf[] = new char[5000];
@@ -89,7 +89,7 @@ public class getAllCollection extends TronlinkBase {
     params.put("address",address721_B58);
     params.put("version","v2");
     response = TronlinkApiList.v2GetAllCollection(params);
-
+    responseContent = TronlinkApiList.parseJsonObResponseContent(response);
     for(int i=0;i<2;i++){
       if (responseContent.getIntValue("code") == 4500){
         Thread.currentThread().sleep(5000);
