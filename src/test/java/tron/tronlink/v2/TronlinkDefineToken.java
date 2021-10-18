e71b4ccc4422475f8b78eefa2bc8ece37083bdd4
wqq <quince.wang@tron.network>
2021-10-18 12:24:40 +0800

    add new branch

---
+++ src/test/java/tron/tronlink/v2/TronlinkDefineToken.java
@@ -0,0 +1,48 @@
+package tron.tronlink.v2;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.http.HttpResponse;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TronlinkDefineToken {
+    private String deployedContract = "TJMZNqLDPLHDMakYxBKonSvd26VdjWHVhP";
+    private JSONObject responseContent;
+    private JSONObject object;
+    private JSONObject dataContent;
+    private HttpResponse response;
+    private JSONArray array = new JSONArray();
+    Map<String, String> params = new HashMap<>();
+
+    @Test(enabled = true)
+    public void Case001SearchBeforeAdd(){
+
+
+    }
+    @Test(enabled = true)
+    public void Case002QueryBeforeAdd(){
+
+    }
+
+    @Test(enabled = true)
+    public void Case003Add(){
+
+    }
+
+    @Test(enabled = true)
+    public void Case004QueryAfterAdd(){
+
+    }
+
+    @Test(enabled = true)
+    public void Case005QueryAfterAdd(){
+
+    }
+
+
+
+
+}
