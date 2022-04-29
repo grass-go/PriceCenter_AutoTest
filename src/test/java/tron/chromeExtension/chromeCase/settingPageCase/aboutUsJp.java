5ebb595f81a1831d23c117d12d0619fff0213eac
wangzichichi <280474930@163.com>
2022-04-29 15:42:43 +0800

    Add retry to about us test case.

--- src/test/java/tron/chromeExtension/chromeCase/settingPageCase/aboutUsJp.java
+++ src/test/java/tron/chromeExtension/chromeCase/settingPageCase/aboutUsJp.java
@@ -9,128 +9,130 @@ import tron.chromeExtension.pages.LoginPage;
 import tron.chromeExtension.pages.MainPage;
 import tron.chromeExtension.pages.SettingPage;
 import tron.chromeExtension.utils.Helper;
+import tron.common.utils.MyIRetryAnalyzer;
 
 public class aboutUsJp extends Base {
 
-  @BeforeMethod
-  public void before() throws Exception {
-    setUpChromeDriver();
-    loginAccount();
-  }
+    @BeforeMethod
+    public void before() throws Exception {
+        setUpChromeDriver();
+        loginAccount();
+    }
 
-  @Test(
-      groups = {"P0"},
-      description = "Change language to Japanese",
-      alwaysRun = true,
-      enabled = true)
-  public void test001changeLanguageToJapaneseTest() throws Exception {
+    @Test(
+            groups = {"P0"},
+            description = "Change language to Japanese",
+            alwaysRun = true,
+            enabled = true,
+            retryAnalyzer = MyIRetryAnalyzer.class)
+    public void test001changeLanguageToJapaneseTest() throws Exception {
 
-    MainPage mainPage = new MainPage(DRIVER);
-    SettingPage settingPage = new SettingPage(DRIVER);
-    waitingTime(5);
-    click(mainPage.set_btn);
-    waitingTime();
-    click(settingPage.settingList.get(4));
-    waitingTime();
-    click(settingPage.Japanese_btn);
-    waitingTime(5);
-    String languageType = getText(settingPage.language_btn);
-    log("languageType:" + languageType);
-    Assert.assertEquals("言語", languageType);
-  }
+        MainPage mainPage = new MainPage(DRIVER);
+        SettingPage settingPage = new SettingPage(DRIVER);
+        waitingTime(5);
+        click(mainPage.set_btn);
+        waitingTime();
+        click(settingPage.settingList.get(4));
+        waitingTime();
+        click(settingPage.Japanese_btn);
+        waitingTime(5);
+        String languageType = getText(settingPage.language_btn);
+        log("languageType:" + languageType);
+        Assert.assertEquals("言語", languageType);
+    }
 
-  @Test(
-      groups = {"P1"},
-      description = "Privacy policy",
-      alwaysRun = true,
-      enabled = true)
-  public void test002privacyPolicyTest() throws Exception {
-    SettingPage settingPage = new SettingPage(DRIVER);
-    String url = Helper.getAboutUsLink(settingPage, 0);
-    log("privacyPolicy_url:" + url);
-    log("privacyReportUrlJp:" + privacyReportUrlJp);
-    Assert.assertEquals(privacyReportUrlJp, url);
-  }
+    @Test(
+            groups = {"P1"},
+            description = "Privacy policy",
+            alwaysRun = true,
+            enabled = true)
+    public void test002privacyPolicyTest() throws Exception {
+        SettingPage settingPage = new SettingPage(DRIVER);
+        String url = Helper.getAboutUsLink(settingPage, 0);
+        log("privacyPolicy_url:" + url);
+        log("privacyReportUrlJp:" + privacyReportUrlJp);
+        Assert.assertEquals(privacyReportUrlJp, url);
+    }
 
-  @Test(
-      groups = {"P1"},
-      description = "Audit report",
-      alwaysRun = true,
-      enabled = true)
-  public void test003auditReportTest() throws Exception {
+    @Test(
+            groups = {"P1"},
+            description = "Audit report",
+            alwaysRun = true,
+            enabled = true)
+    public void test003auditReportTest() throws Exception {
 
-    SettingPage settingPage = new SettingPage(DRIVER);
-    String url = Helper.getAboutUsLink(settingPage, 1);
-    log("auditReport_url:" + url);
-    log("auditReport:" + auditReport);
-    Assert.assertEquals(auditReport, url);
-  }
+        SettingPage settingPage = new SettingPage(DRIVER);
+        String url = Helper.getAboutUsLink(settingPage, 1);
+        log("auditReport_url:" + url);
+        log("auditReport:" + auditReport);
+        Assert.assertEquals(auditReport, url);
+    }
 
-  @Test(
-      groups = {"P1"},
-      description = "Website",
-      alwaysRun = true,
-      enabled = true)
-  public void test004websiteTest() throws Exception {
+    @Test(
+            groups = {"P1"},
+            description = "Website",
+            alwaysRun = true,
+            enabled = true)
+    public void test004websiteTest() throws Exception {
 
-    SettingPage settingPage = new SettingPage(DRIVER);
-    String url = Helper.getAboutUsLink(settingPage, 2);
-    log("website_url:" + url);
-    log("website:" + website);
-    Assert.assertEquals(website, url);
-  }
+        SettingPage settingPage = new SettingPage(DRIVER);
+        String url = Helper.getAboutUsLink(settingPage, 2);
+        log("website_url:" + url);
+        log("website:" + website);
+        Assert.assertEquals(website, url);
+    }
 
-  @Test(
-      groups = {"P1"},
-      description = "Support center",
-      alwaysRun = true,
-      enabled = true)
-  public void test005supportCenterTest() throws Exception {
+    @Test(
+            groups = {"P1"},
+            description = "Support center",
+            alwaysRun = true,
+            enabled = true)
+    public void test005supportCenterTest() throws Exception {
 
-    SettingPage settingPage = new SettingPage(DRIVER);
-    String url = Helper.getAboutUsLink(settingPage, 3);
-    log("SupportCenter_url:" + url);
-    log("supportCenter:" + supportCenter);
-    Assert.assertEquals(supportCenter, url);
-  }
+        SettingPage settingPage = new SettingPage(DRIVER);
+        String url = Helper.getAboutUsLink(settingPage, 3);
+        log("SupportCenter_url:" + url);
+        log("supportCenter:" + supportCenter);
+        Assert.assertEquals(supportCenter, url);
+    }
 
-  @Test(
-      groups = {"P1"},
-      description = "Contact us.",
-      alwaysRun = true,
-      enabled = true)
-  public void test006contactusTest() throws Exception {
+    @Test(
+            groups = {"P1"},
+            description = "Contact us.",
+            alwaysRun = true,
+            enabled = true)
+    public void test006contactusTest() throws Exception {
 
-    SettingPage settingPage = new SettingPage(DRIVER);
-    String url = Helper.getAboutUsLink(settingPage, 4);
-    log("Contactus_url:" + url);
-    log("contactUs:" + contactUs);
-    Assert.assertEquals(contactUs, url);
-  }
+        SettingPage settingPage = new SettingPage(DRIVER);
+        String url = Helper.getAboutUsLink(settingPage, 4);
+        log("Contactus_url:" + url);
+        log("contactUs:" + contactUs);
+        Assert.assertEquals(contactUs, url);
+    }
 
-  @Test(
-      groups = {"P0"},
-      description = "Change language to Chinese",
-      alwaysRun = true,
-      enabled = true)
-  public void test007changeLanguageToChineseTest() throws Exception {
+    @Test(
+            groups = {"P0"},
+            description = "Change language to Chinese",
+            alwaysRun = true,
+            enabled = true)
+    public void test007changeLanguageToChineseTest() throws Exception {
 
-    MainPage mainPage = new MainPage(DRIVER);
-    SettingPage settingPage = new SettingPage(DRIVER);
-    waitingTime(5);
-    click(mainPage.set_btn);
-    waitingTime();
-    click(settingPage.settingList.get(4));
-    waitingTime(5);
-    click(settingPage.Chinese_btn);
-    waitingTime(5);
-    String languageType = getText(settingPage.language_btn);
-    log("languageType:" + languageType);
-    Assert.assertEquals("语言", languageType);
-  }
+        MainPage mainPage = new MainPage(DRIVER);
+        SettingPage settingPage = new SettingPage(DRIVER);
+        waitingTime(5);
+        click(mainPage.set_btn);
+        waitingTime();
+        click(settingPage.settingList.get(4));
+        waitingTime(5);
+        click(settingPage.Chinese_btn);
+        waitingTime(5);
+        String languageType = getText(settingPage.language_btn);
+        log("languageType:" + languageType);
+        Assert.assertEquals("语言", languageType);
+    }
 
-  @AfterMethod(enabled = true)
-  public void after() throws Exception {
-    logoutAccount();
-  }
+    @AfterMethod(enabled = true)
+    public void after() throws Exception {
+        logoutAccount();
+    }
 }
