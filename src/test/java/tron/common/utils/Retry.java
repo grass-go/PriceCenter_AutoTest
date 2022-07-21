ccba9cc88cef7a335299abdda650065de9442874
wqq <quince.wang@tron.network>
2022-07-21 14:49:55 +0800

    modify retry to 5 times and interval is 30s

--- src/test/java/tron/common/utils/Retry.java
+++ src/test/java/tron/common/utils/Retry.java
@@ -6,7 +6,7 @@ import org.testng.ITestResult;
 public class Retry implements IRetryAnalyzer {
 
   private int retryCount = 0;
-  private int maxRetryCount = 1;
+  private int maxRetryCount = 5;
 
   // Below method returns 'true' if the test method has to be retried else 'false'
   //and it takes the 'Result' as parameter of the test method that just ran
@@ -21,7 +21,7 @@ public class Retry implements IRetryAnalyzer {
           + getResultStatusName(result.getStatus()) + " for the " + (retryCount + 1) + " time(s).");
       retryCount++;
       try {
-        Thread.sleep(3000);
+        Thread.sleep(30000);
       } catch (InterruptedException e) {
         e.printStackTrace();
       }
