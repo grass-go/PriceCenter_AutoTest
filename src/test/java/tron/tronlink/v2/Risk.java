19246b49c983f6a7d2c4c2d6dfed43f5075f2038
danny.guo <danny.guo@tron.network>
2022-07-27 17:07:29 +0800

    fix bug

--- src/test/java/tron/tronlink/v2/Risk.java
+++ src/test/java/tron/tronlink/v2/Risk.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import tron.common.TronlinkApiList;
@@ -13,44 +14,75 @@ import tron.tronlink.base.TronlinkBase;
 import java.util.*;
 
 @Slf4j
-public class Rick extends TronlinkBase {
+public class Risk extends TronlinkBase {
 
     private RiskTokens riskTokens = new RiskTokens();
+    private Random ramdom = new Random();
 
     @Test(description = "判断risktokens接口返回的token在该接口能否查到")
     public void getRisk(){
         JSONObject tokenObjs = riskTokens.GetAllRiskTokens();
         JSONArray arr = tokenObjs.getJSONArray(Keys.data);
         List<String> tokenStrs = JSONObject.parseArray(arr.toJSONString(), String.class);
-        for (String str:
-             tokenStrs) {
-            Assert.assertEquals(true, isRiskToken(str));
 
-            break;
+        // 挑选10-20条数据然后请求
+        List<String> pickTokens = generateRandomTokens(tokenStrs);
+        for (String str:
+             pickTokens) {
+            Assert.assertEquals(true, isRiskToken(str));
         }
+        // 构造错误的地址
+        List<String> errTokens = generateErrTokens();
+        for (String str:
+                errTokens) {
+//            Assert.assertEquals(false, isRiskToken(str));
+        }
+    }
+
+    // 测试数据：rsp边缘数据+随机抽取数据
+    private List<String> generateRandomTokens(List<String> tokens){
+        int totalTokenNum = 15;
+        Assert.assertTrue(tokens.size() > totalTokenNum);
+        List<String> result = tokens.subList(0, 5);
+        result.addAll(tokens.subList(tokens.size() - 5,tokens.size()));
+        for (int i = 0; i < 5; i++) {
+            result.add(tokens.get(ramdom.nextInt(tokens.size())));
+        }
+
+        log.info("prepare get rick token correct data: " +result);
+        return  result;
+    }
+
+    private List<String> generateErrTokens(){
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            result.add(UUID.randomUUID().toString().replace("-", "").substring(0, 35).toUpperCase());
+        }
+        log.info("prepare get rick token err data: " +result);
+        return  result;
+
     }
 
 
     private boolean isRiskToken(String token){
         final String url = "/api/wallet/v2/risk";
-        final String method = "POST";
+        final String method = "GET";
 
-        Map<String,String> params = GenerateParams(queryAddress58, url, method);
+        Map<String,String> params = GenerateParams(queryAddress58,token, url, method);
         Map<String,String> headers = GenerateHeaders();
-        JSONObject body = new JSONObject();
-        body.put(Keys.Address, queryAddress58);
-        HttpResponse response = TronlinkApiList.v2GetRisk(params, body, headers, url);
+        HttpResponse response = TronlinkApiList.v2GetRisk(params, null, headers, url);
         Assert.assertNotEquals(response, null);
+        Assert.assertEquals(response.getStatusLine().getStatusCode(), HttpStatus.SC_OK);
         JSONObject riskRsp = TronlinkApiList.parseJsonObResponseContent(response);
         log.debug("all risk tokens =" + riskRsp.toJSONString());
-
         return false;
     }
 
-    private Map<String,String> GenerateParams(String Address, String url, String method){
+    private Map<String,String> GenerateParams(String Address,String token, String url, String method){
         Map<String,String> params = new HashMap<>();
         params.put("nonce","12345");
         params.put("secretId","SFSUIOJBFMLKSJIF");
+        params.put("tokenAddress", token);
         // 计算sig
         HashMap<String,String> sigs = new HashMap<>();
         sigs.put("address", Address);
@@ -71,6 +103,8 @@ public class Rick extends TronlinkBase {
         Map<String,String> headers = TronlinkApiList.getV2Header();
         headers.put("deviceName", "wqq");
         headers.put("osVersion", "10");
+        headers.remove("Connection");
+
         log.info("headers : " + headers);
         return headers;
     }
