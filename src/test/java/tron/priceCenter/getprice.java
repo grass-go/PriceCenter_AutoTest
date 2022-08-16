45e49361fd759f52ec59def2d95ffcad43fad9f4
wqq <quince.wang@tron.network>
2022-08-16 17:51:14 +0800

    add dup cases

--- src/test/java/tron/priceCenter/getprice.java
+++ src/test/java/tron/priceCenter/getprice.java
@@ -36,63 +36,14 @@ public class getprice {
     @BeforeClass(enabled = true,description = "getallprice")
     //@Test(enabled = true, description = "")
     public void prepareTokenExpPriceFromOtherPlat() {
-        PriceCenterApiList.prepareExchangeTokensAndPrice();
-        PriceCenterApiList.preparejTokensAndPrice();
+        //PriceCenterApiList.prepareExchangeTokensAndPrice();
+        //PriceCenterApiList.preparejTokensAndPrice();
         PriceCenterApiList.SetTrxPriceMap();
 
     }
 
-    public void compareTokensPriceWithTronscan(Map<String,String> tokenAddressMap, Map<String,String> tokenExpPrice) {
-        long curTime = System.currentTimeMillis();
-        log.info("startTime:"+curTime);
-        Map<String,String> priceCentreMap= new HashMap<>();
-        String symbol=null;
-        String symbolParamValue = "";
-        for (Map.Entry<String, String> entry : tokenAddressMap.entrySet()) {
-            symbol = entry.getKey();
-            symbolParamValue = symbolParamValue+symbol+",";
-        }
-        symbolParamValue = symbolParamValue.substring(0,symbolParamValue.length()-1);
-
-        //get ExchangeToken Price From price-centre.
-        params.clear();
-        params.put("symbol", symbolParamValue);
-        params.put("convert", "TRX");
-        response = PriceCenterApiList.getprice(params);
-        Assert.assertEquals(200, response.getStatusLine().getStatusCode());
-        responseContent = TronlinkApiList.parseJsonObResponseContent(response);
-        for (Map.Entry<String, String> entry : tokenExpPrice.entrySet()) {
-            symbol = entry.getKey();
-            String expPrice = entry.getValue();
-            log.info("compareTokensPriceWithTronscan using TRX price unit: cur symbol:"+symbol);
-            Object curPrice = JSONPath.eval(responseContent, "$..data."+symbol+".quote.TRX.price[0]");
-            String priceCentreValue =curPrice.toString();
-            log.info("compareTokensPriceWithTronscan using TRX price unit: expPrice:"+expPrice+",priceCentreValue:"+priceCentreValue);
-
-            Assert.assertTrue(PriceCenterApiList.CompareGapInGivenTolerance(expPrice, priceCentreValue,"0.2"));
-
-            Object curPriceTime = JSONPath.eval(responseContent, "$..data."+symbol+".quote.TRX.last_updated[0]");
-            log.info("symbol:"+symbol+", curPriceTime:"+ curPriceTime);
-        }
-
-        Object statusCode = JSONPath.eval(responseContent, "$..status.error_code[0]");
-        Assert.assertEquals(0,statusCode);
-        Object error_msg = JSONPath.eval(responseContent, "$..status.error_message[0]");
-        Assert.assertNull(error_msg);
-    }
-
-    @Test(enabled = true, description = "Check price of exchange Tokens from Price-Centre by symbol is the same as tronscan")
-    public void Test001CheckCentrePrice_ExgTokens_Tronscan() {
-        compareTokensPriceWithTronscan(PriceCenterApiList.ExgTokenAddressMap, PriceCenterApiList.exp_exgTokenValueMap);
-    }
-
-    @Test(enabled = true, description = "Check price of jToken from Price-Centre by symbol is the same as tronscan")
-    public void Test002CheckCentrePrice_jTokens_Tronscan() {
-        compareTokensPriceWithTronscan(PriceCenterApiList.JTokenAddressMap,PriceCenterApiList.exp_JTokenValueMap);
-    }
-
     @Test(enabled = true, description = "Check TRX/usd Price with price got from CMC")
-    public void Test003CheckCentrePrice_TrxRelated_CMC() throws URISyntaxException {
+    public void Test001CheckCentrePrice_TrxRelated_CMC() throws URISyntaxException {
         String expTrxPrice = PriceCenterApiList.trxPriceMap.get("USD");
         params.clear();
         params.put("symbol", "TRX,WTRX,jTRX");
@@ -122,7 +73,7 @@ public class getprice {
 
 
     @Test(enabled = true, description = "Check BTT price with swap api")
-    public void Test004CheckCentrePrice_BTT_SunSwap(){
+    public void Test002CheckCentrePrice_BTT_SunSwap(){
         String swapAPI = "https://pabc.ablesdxd.link/swapv2/scan/getTokenInfo?tokenAddress=TAFjULxiVgT4qWk6UZwjqwZXTSaGaqnVp4";
         response = PriceCenterApiList.createGetConnectWithHeader(swapAPI,null,null,null);
         responseContent = TronlinkApiList.parseJsonObResponseContent(response);
@@ -139,7 +90,7 @@ public class getprice {
     //WBTT, jWBTT, BTT, BTTOLD
     //真实价格的比较已经在getallprice有，预期是自己设定的固定值。此处仅对getprice内各币关系做比较。
     @Test(enabled = true, description = "Check BTT related price")
-    public void Test005CheckRelationship_BttRelated() throws URISyntaxException {
+    public void Test003CheckRelationship_BttRelated() throws URISyntaxException {
         //check WBTT = BTTOLD , BTTOLD=BTT*1000, WBTT=jWBTT*100
         params.clear();
         params.put("symbol", "WBTT,BTTOLD,jWBTT,BTT,jBTT");
@@ -167,7 +118,7 @@ public class getprice {
     //WBTT价格上面已单独计算，目前仅剩WETH价格。
     //目前价格中心WETH的价格也没有。所以case暂时false。
     @Test(enabled = false, description = "Check wToken's price with related token by usd price")
-    public void Test006CheckDiffFormat_wTokens() throws URISyntaxException {
+    public void Test004CheckDiffFormat_wTokens() throws URISyntaxException {
         Map<String,String> wTokenMap = new HashMap<>();
         wTokenMap.put("WETH","ETH");
         java.util.List joinedStr = PriceCenterApiList.getJoinedSymbolAndAddress(wTokenMap);
