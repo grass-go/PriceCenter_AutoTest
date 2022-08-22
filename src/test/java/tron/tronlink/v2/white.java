dcf882f1cfb81e516b7aa022e6d04e26f095dbfe
wqq <quince.wang@tron.network>
2022-08-22 15:22:32 +0800

    add white api case

--- src/test/java/tron/tronlink/v2/white.java
+++ src/test/java/tron/tronlink/v2/white.java
@@ -23,7 +23,7 @@ public class white extends TronlinkBase {
 
 
     @Test(enabled = true)
-    public void test01CheckWhiteStatus() throws Exception {
+    public void test01CheckInWhite() throws Exception {
 
         param.put("contract_address","TXXD6jwUYdPRcMA3CP6gCWPQPY7LwrD5mZ");
         param.put("nonce","12345");
@@ -41,9 +41,32 @@ public class white extends TronlinkBase {
         Assert.assertEquals("TXXD6jwUYdPRcMA3CP6gCWPQPY7LwrD5mZ", dataContent.getString("contractAddress"));
         Assert.assertEquals("SUN", dataContent.getString("projectName"));
         Assert.assertEquals("sun.io", dataContent.getString("projectUrl"));
+        Assert.assertEquals("https://static.tronscan.org/production/upload/logo/sun10.png", dataContent.getString("projectLogo"));
         Assert.assertTrue(dataContent.getBooleanValue("white"));
 
     }
 
+    @Test(enabled = true)
+    public void test01CheckNotInWhite() throws Exception {
+
+        param.put("contract_address", "TE2VpPmPQp9UZpDPcBS4G8Pw3R1BZ4Ea6j");
+        param.put("nonce", "12345");
+        param.put("secretId", "SFSUIOJBFMLKSJIF");
+        param.put("signature", "DgRWiagEjwyFqgYOV7Smcqe%2FtsU%3D");
+        param.put("address", quince_B58);
+
+        response = TronlinkApiList.v2CheckWhite(param);
+        Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
+        responseContent = TronlinkApiList.parseJsonObResponseContent(response);
+        TronlinkApiList.printJsonContent(responseContent);
+        Assert.assertTrue(responseContent.getInteger("code") == 0);
+        Assert.assertEquals(responseContent.getString("message"), "OK");
+        JSONObject dataContent = responseContent.getJSONObject("data");
+        Assert.assertEquals("", dataContent.getString("contractAddress"));
+        Assert.assertEquals("", dataContent.getString("projectName"));
+        Assert.assertEquals("", dataContent.getString("projectUrl"));
+        Assert.assertEquals("", dataContent.getString("projectLogo"));
+        Assert.assertFalse(dataContent.getBooleanValue("white"));
+    }
 
 }
