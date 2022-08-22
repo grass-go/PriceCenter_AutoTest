2807cf6dba1ef3a982b990997591d7f024b90894
danny.guo <danny.guo@tron.network>
2022-08-22 19:00:19 +0800

    添加获取dapp白名单自动化case

---
+++ src/test/java/tron/tronlink/dapp/model/Data.java
@@ -0,0 +1,29 @@
+package tron.tronlink.dapp.model;
+
+public class Data {
+
+    private String url;
+    private String icon;
+    private String name;
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    public String getUrl() {
+        return url;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+}
