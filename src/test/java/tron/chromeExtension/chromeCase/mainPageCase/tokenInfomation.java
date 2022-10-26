afc78a5ce82cc4c3c69e0fa15cae2dde9888290c
liqi <1242201319@qq.com>
2022-10-26 15:06:46 +0800

    add test case.

--- src/test/java/tron/chromeExtension/chromeCase/mainPageCase/tokenInfomation.java
+++ src/test/java/tron/chromeExtension/chromeCase/mainPageCase/tokenInfomation.java
@@ -9,6 +9,7 @@ import org.testng.annotations.Test;
 import tron.chromeExtension.base.Base;
 import tron.chromeExtension.pages.AllAssetsPage;
 import tron.chromeExtension.pages.MainPage;
+import tron.chromeExtension.utils.Helper;
 
 public class tokenInfomation extends Base {
 
@@ -42,7 +43,7 @@ public class tokenInfomation extends Base {
       description = "Verify that the TRX icon has a Dominic French Coins flag.",
       alwaysRun = true,
       enabled = true)
-  public void test002TRXIconHasADominicFrenchCoinsFlag() throws Exception {
+  public void test002TokenFromHomePageIconHasADominicFrenchCoinsFlag() throws Exception {
     MainPage mainPage = new MainPage(DRIVER);
     waitingTime(3);
     for (int i = 0; i < mainPage.token_list.size(); i++) {
@@ -53,6 +54,28 @@ public class tokenInfomation extends Base {
               .findElement(new By.ByClassName("nameWrap"))
               .findElements(By.className("nationalFlag")));
     }
+  }
+
+  @Test(
+      groups = {"P0"},
+      description = "Verify that the TRX icon has a Dominic French Coins flag.",
+      alwaysRun = true,
+      enabled = true)
+  public void test003TokenFromSearchIconHasADominicFrenchCoinsFlag() throws Exception {
+    MainPage mainPage = new MainPage(DRIVER);
+    AllAssetsPage allAssetsPage = new AllAssetsPage(DRIVER);
+    waitingTime(3);
+    click(mainPage.allAssets_btn);
+    waitingTime(3);
+    Helper.click(allAssetsPage.tokenSearch_input);
+    waitingTime(3);
+    sendKeys(allAssetsPage.tokenSearch1_input, "USDD");
+    WebElement trxDiv = mainPage.token_list.get(0);
+    waitingTime(3);
+    Assert.assertNotNull(
+        trxDiv
+            .findElement(new By.ByClassName("nameWrap"))
+            .findElements(By.className("nationalFlag")));
   }
 
   @AfterMethod(enabled = true)
