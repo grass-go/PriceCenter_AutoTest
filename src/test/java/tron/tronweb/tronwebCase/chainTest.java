ac00c5abff25e6b28b482354112d4e627ccbee51
wangqqqqq <wangqianqian@tron.network>
2022-11-02 18:36:22 +0800

    Improve tronweb test case

--- src/test/java/tron/tronweb/tronwebCase/chainTest.java
+++ src/test/java/tron/tronweb/tronwebCase/chainTest.java
@@ -186,9 +186,9 @@ public class chainTest extends Base {
         Assert.assertEquals("41f596e85bfd042744f76880979a133da0728679d9",object.getString("creator_address"));
         Assert.assertEquals(1539673398000L,object.getLongValue("create_time"));
         Assert.assertEquals("31303030353634",object.getString("first_token_id"));
-        Assert.assertEquals(8,object.getLongValue("first_token_balance"));
+        Assert.assertEquals(5,object.getLongValue("first_token_balance"));
         Assert.assertEquals("5f",object.getString("second_token_id"));
-        Assert.assertEquals(2326459,object.getLongValue("second_token_balance"));
+        Assert.assertEquals(4326459,object.getLongValue("second_token_balance"));
     }
 
     @Test(enabled = true, description = "Test timeUntilNextVoteCycle")
